5e12fe2092e83ec76ad135be1e532f61585c7978
virunpatch <guyver125@hotmail.com>
2021-02-23 19:24:41 +0700

    commit

--- src/contents/Home.js
+++ src/contents/Home.js
@@ -7,7 +7,7 @@ class Home extends Component {
     render() {
         return (
             <div className="condiv home">
-                <img src="https://i.pinimg.com/564x/69/f4/ff/69f4ff2f8b79570b0018914ec5734ccd.jpg" alt="profile" className="profilepic" />
+                <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/7a844829-08b7-48eb-94f6-83b0014c5576/de9ufql-8e42128d-1c38-48de-b904-eb7181caf294.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvN2E4NDQ4MjktMDhiNy00OGViLTk0ZjYtODNiMDAxNGM1NTc2XC9kZTl1ZnFsLThlNDIxMjhkLTFjMzgtNDhkZS1iOTA0LWViNzE4MWNhZjI5NC5naWYifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.AkO5I7-rMQJcwl3u-mrhWmbljZyF1UObgmBLsj0GEyM" alt="profile" className="profilepic" />
                 <ReactTypingEffect text={['I am Virunpatch', 'I am a Web Developer']} speed={80} eraseDelay={200} className="typingeffect" />
                 <Social />
             </div>
